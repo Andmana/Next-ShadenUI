c37a11be157160a203309be6bf610ec549e47688
andmana <iam.andmana@gmail.com>
2025-05-12 13:03:22 +0700

    At least work before change

--- app/articles/ArticlesContent.jsx
+++ app/articles/ArticlesContent.jsx
@@ -8,7 +8,7 @@ const DEFAULT_LIMIT = "9";
 
 const ArticlesContent = async ({ searchParams }) => {
   // Validate and sanitize searchParams
-  const { page, limit, category, title } = searchParams || {};
+  const { page, limit, category, title } = (await searchParams) || {};
 
   // Construct query parameters safely
   const queryParams = new URLSearchParams();
@@ -52,6 +52,13 @@ const ArticlesContent = async ({ searchParams }) => {
             ))}
           </div>
         )}
+        <div className=" w-full mt-9 flex justify-center items-center gap-2 text-slate-900">
+          <Pagin
+            itemsCount={articles.length}
+            limit={limit || DEFAULT_LIMIT}
+            page={page || DEFAULT_PAGE}
+          />
+        </div>
       </>
     );
   } catch (error) {
--- components/pagination/Pagination.jsx
+++ components/pagination/Pagination.jsx
@@ -1,19 +1,68 @@
+"use client";
+
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import { Button } from "../ui/button";
+import { ChevronRight } from "lucide-react";
+import { ChevronLeft } from "lucide-react";
+import { useSearchParams } from "next/navigation";
+
 const Pagin = ({ page, itemsCount, limit }) => {
-  const havePrevious = parseInt(page) > 1;
+  const searchParams = useSearchParams();
+  // Check if there's a next page
+  const hasNext = parseInt(itemsCount) === parseInt(limit);
+  // Check if there's a previous page
+  const hasPrevious = parseInt(page) > 1;
+
+  const nextPageParams = new URLSearchParams(searchParams);
+  nextPageParams.set("page", parseInt(page) + 1);
+
+  const prevPageParams = new URLSearchParams(searchParams);
+  prevPageParams.set("page", parseInt(page) - 1);
+
   return (
     <Pagination>
       <PaginationContent>
-        <PaginationItem disable={havePrevious}>
-          <PaginationPrevious href="#" />
+        {/* Disable PaginationPrevious if there's no previous page */}
+        <PaginationItem>
+          {hasPrevious ? (
+            <PaginationPrevious
+              href={`/articles?${prevPageParams.toString()}`}
+            />
+          ) : (
+            <Button variant="primary" disabled>
+              <ChevronLeft /> Previous
+            </Button>
+          )}
         </PaginationItem>
+
+        {/* Current page */}
         <PaginationItem>
           <PaginationLink href="#">{page}</PaginationLink>
         </PaginationItem>
+
+        {/* Pagination Ellipsis */}
         <PaginationItem>
           <PaginationEllipsis />
         </PaginationItem>
+
+        {/* Disable PaginationNext if there's no next page */}
+
         <PaginationItem>
-          <PaginationNext href="#" />
+          {hasNext ? (
+            <PaginationNext href={`/articles?${nextPageParams.toString()}`} />
+          ) : (
+            <Button variant="primary" disabled>
+              Next <ChevronRight />
+            </Button>
+          )}
         </PaginationItem>
       </PaginationContent>
     </Pagination>

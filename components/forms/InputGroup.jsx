7b92dfeb4ddc5e82349c8d2f365a76135ec6b032
andmana <iam.andmana@gmail.com>
2025-05-13 19:55:07 +0700

    Install Quilli

--- components/forms/InputGroup.jsx
+++ components/forms/InputGroup.jsx
@@ -1,12 +1,12 @@
 import { Label } from "../ui/label";
 
 const InputGroup = ({ children, label }) => {
-    return (
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label htmlFor={label.toString().toLowerCase()}>{label}</Label>
-            {children}
-        </div>
-    );
+  return (
+    <div className="grid w-full items-center gap-1.5">
+      <Label htmlFor={label.toString().toLowerCase()}>{label}</Label>
+      {children}
+    </div>
+  );
 };
 
 export default InputGroup;
